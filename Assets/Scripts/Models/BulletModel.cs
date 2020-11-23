a0e3fb2fb2dcb7612a8e2a91c57987884d50f6e4
Виталий Лукин <mr.lukin1990@mail.ru>
2020-11-23 13:30:54 +0300

    Create Shader SkyboxStar

--- Assets/Scripts/Models/BulletModel.cs
+++ Assets/Scripts/Models/BulletModel.cs
@@ -15,5 +15,10 @@ public class BulletModel : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+
+        if (transform.position.y > 10f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
